ccb24c232ca5d51baac1ee25b7e7add6d39ee6c3
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2018-05-13 18:28:42 -0300

    [Add] Repository and Service to Console.

--- RoomBooking.Console/Program.cs
+++ RoomBooking.Console/Program.cs
@@ -1,4 +1,12 @@
-﻿using Unity;
+﻿using RoomBooking.Console.Repositories;
+using RoomBooking.Console.Services;
+using RoomBooking.Domain.Contracts.Repositories;
+using RoomBooking.Domain.Contracts.Services;
+using RoomBooking.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using Unity;
+using Unity.Lifetime;
 
 namespace RoomBooking.Console
 {
@@ -6,11 +14,34 @@ namespace RoomBooking.Console
     {
         static void Main(string[] args)
         {
-            var container = new UnityContainer();
+            //var container = new UnityContainer();
 
-            container.RegisterType<IAuthorRepository, AuthorRepository>(new HierarchicalLifetimeManager());
+            //container.RegisterType<IBookRepository, BookRepository>(new HierarchicalLifetimeManager());
+            //container.RegisterType<IBookService, BookService>(new HierarchicalLifetimeManager());
 
-            var productRepository = container.Resolve<IProductRepository>();
+            //var bookRepository = container.Resolve<IBookRepository>();
+            //var bookService = container.Resolve<IBookService>();
+
+            //var room = new Room(DateTime.Now, DateTime.Now.AddHours(8), "Sala 1");
+
+            //bookService.PlaceBook(DateTime.Now.AddHours(2), DateTime.Now.AddHours(3), room.Id);
+
+            var room = new Room(DateTime.Now.AddHours(2), DateTime.Now.AddHours(8), "Sala 1");
+            System.Console.WriteLine("Criando uma sala");
+
+            var book = new Book(room, DateTime.Now.AddHours(4), DateTime.Now.AddHours(5));
+            System.Console.WriteLine("Iniciando uma reserva");
+
+            book.Confirm(new List<DateTime>(), new List<DateTime>());
+            System.Console.WriteLine("Status da reserva: " + book.Status);
+
+            book.MarkAsInProgress();
+            System.Console.WriteLine("Status da reserva: " + book.Status);
+
+            book.MarkAsCompleted();
+            System.Console.WriteLine("Status da reserva: " + book.Status);
+
+            System.Console.Read();
         }
     }
 }
---
+++ RoomBooking.Console/Services/BookService.cs
@@ -0,0 +1,24 @@
+﻿using RoomBooking.Domain.Contracts.Services;
+using RoomBooking.Domain.Entities;
+using System;
+
+namespace RoomBooking.Console.Services
+{
+    public class BookService : IBookService
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlaceBook(DateTime startDate, DateTime endDate, Guid room)
+        {
+            //var book = new Book(room, DateTime.Now.AddHours(1), DateTime.Now.AddHours(2));
+
+            //book.Cancel();
+
+            //bookRepository.Save(book);
+            //throw new NotImplementedException();
+        }
+    }
+}
